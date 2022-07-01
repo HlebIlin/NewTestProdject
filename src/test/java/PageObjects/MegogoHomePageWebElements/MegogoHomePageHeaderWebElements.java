fe9bec8232f99fb8fec7881f39115e56ff24e5be
Max <chirvamaxym@gmail.com>
2022-07-01 16:29:59 +0300

    add Home page Header Web Elements

---
+++ src/test/java/PageObjects/MegogoHomePageWebElements/MegogoHomePageHeaderWebElements.java
@@ -0,0 +1,14 @@
+package PageObjects.MegogoHomePageWebElements;
+
+import PageObjects.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class MegogoHomePageHeaderWebElements extends BasePage {
+
+    public MegogoHomePageHeaderWebElements(WebDriver driver) {
+        super(driver);
+    }
+
+
+
+}
